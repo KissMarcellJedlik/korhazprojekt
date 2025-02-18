35e6fa68c3fe417f6199382802cfd1349843ac4c
Kiss Marcell <kiss.marell1@students.jedlik.eu>
2025-02-18 23:52:41 +0100

    osztaly class modositva

--- korhazprojekt/korhazprojekt/Osztaly.cs
+++ korhazprojekt/korhazprojekt/Osztaly.cs
@@ -9,18 +9,16 @@ namespace korhazprojekt
     public class Osztaly
     {
         public string Nev { get; set; }
-        private List<Beteg> Betegek { get; set; }
+        public List<Beteg> Betegek { get; set; } = new List<Beteg>();
 
         public Osztaly(string nev)
         {
             Nev = nev;
-            Betegek = new List<Beteg>();
         }
 
         public void FelveszBeteget(Beteg beteg)
         {
             Betegek.Add(beteg);
-            Console.WriteLine($"{beteg.Nev} felvéve a {Nev} osztályra.");
         }
     }
 }
